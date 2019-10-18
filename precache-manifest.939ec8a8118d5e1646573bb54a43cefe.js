3214f49c33dd451c344d38ff253d8ab3d4aad511
Eileen Kang <kmye@live.com>
2019-10-18 21:25:43 +0800

    Updates

---
+++ precache-manifest.939ec8a8118d5e1646573bb54a43cefe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1dadfc4770b8aca8212e1a0d47b103",
+    "url": "/ulala-clan-war-planner/index.html"
+  },
+  {
+    "revision": "ad375e1663b357c936a6",
+    "url": "/ulala-clan-war-planner/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "936e1b18f08afd45e56a",
+    "url": "/ulala-clan-war-planner/static/css/main.94bf3e33.chunk.css"
+  },
+  {
+    "revision": "ad375e1663b357c936a6",
+    "url": "/ulala-clan-war-planner/static/js/2.685bf286.chunk.js"
+  },
+  {
+    "revision": "936e1b18f08afd45e56a",
+    "url": "/ulala-clan-war-planner/static/js/main.5288e302.chunk.js"
+  },
+  {
+    "revision": "7936fe462caf2805fcb7",
+    "url": "/ulala-clan-war-planner/static/js/runtime-main.a2004ae2.js"
+  }
+]);
